a2ad830a14d3d3c7252d15b678c8c2f8d9cc7651
ltx <2108540568@qq.com>
2026-04-02 00:39:13 +0800

    tcp header donw

--- server/common/internal_tcp.hh
+++ server/common/internal_tcp.hh
@@ -2,6 +2,7 @@
 
 
 #include <boost/asio.hpp>
+#include <future>
 #include <memory>
 #include <unordered_set>
 
@@ -16,10 +17,9 @@ class _channel : public std::enable_shared_from_this<_channel>{
     friend class InternalTcp;
 public:
     _channel(btcp::socket&& socket)
-    :socket_(std::move(socket))
+        :socket_(std::move(socket))
     {}
     void do_read(){
-        
         read_buffer_.resize(read_size_ + DEFAULT_READ_BUFFER_SIZE);
         socket_.async_read_some(asio::buffer(read_buffer_.data() + read_size_, DEFAULT_READ_BUFFER_SIZE),[this](boost::system::error_code ec, std::size_t bytes_transferred){
             if(!ec){
@@ -42,10 +42,15 @@ public:
             do_read();
         });
     }
-    void do_write(std::shared_ptr<std::vector<uint8_t>> data){
+    // thread not safe
+    void do_write(const std::string& data){
+        do_write(data.c_str(),data.size());
+    }
+    void do_write(const void* data, size_t size){
+        write_buffer_.resize(write_size_ + size);
         if (data) {
-            std::copy(data->data(),data->data() + data->size(),write_buffer_.data() + write_size_);
-            write_size_ += data->size();
+            std::copy((char*)data,(char*)data + size,write_buffer_.data() + write_size_);
+            write_size_ += size;
         }
         asio::async_write(
             socket_, 
@@ -56,7 +61,7 @@ public:
                 // 处理写入完成
                 std::copy(write_buffer_.data() + bytes_transferred,write_buffer_.data() + write_size_,write_buffer_.data());
                 write_size_ -= bytes_transferred;
-                if (write_size_ == 0){
+                if (write_size_ > 0){
                     do_write(nullptr);
                 }
             }else {
@@ -65,14 +70,16 @@ public:
             }
         });
     }
+    btcp::endpoint endpoint(){
+        return socket_.remote_endpoint();
+    }
 private:
     void do_close(){
-        if (close_callback_) {
-            close_callback_(shared_from_this());
+        if (connection_callback_) {
+            connection_callback_(shared_from_this(),false,"");
         }
         socket_.close();
         remove_callback_(shared_from_this());
-        
     }
 
 private:
@@ -81,8 +88,8 @@ private:
     std::vector<uint8_t> write_buffer_;
     uint32_t read_size_{0};
     uint32_t write_size_{0};
-    std::function<int32_t(std::shared_ptr<_channel>,void*,uint32_t)> read_callback_;
-    std::function<void(std::shared_ptr<_channel>)> close_callback_;
+    std::function<uint32_t(std::shared_ptr<_channel>,void*,uint32_t)> read_callback_;
+    std::function<void(std::shared_ptr<_channel>,bool,const std::string&)> connection_callback_;
     std::function<void(std::shared_ptr<_channel>)> remove_callback_;
 }; // class _channel
 using Channel = std::shared_ptr<_channel>;
@@ -91,28 +98,80 @@ class InternalTcp{
 public:
     InternalTcp(int port)
     :io_context_(),
-    acceptor_(io_context_, btcp::v4(),port)
+    acceptor_(io_context_, btcp::endpoint(btcp::v4(),port))
     {
-        
+        do_accept();
     }
     ~InternalTcp() = default;
+    void sync_start(){
+        io_context_.run();
+    }
+    void async_start(){
+        thread_ = std::make_shared<std::thread>([this](){
+            io_context_.run();
+            end_.set_value(true);
+        });
+        thread_->detach();
+    }
+    void connect(const std::string& ip,int port,const std::string& flag = ""){
+        io_context_.post([this,ip,port,flag](){
+            do_connect(ip,port,flag);
+        });
+    }
+    void disconnect(Channel channel){
+        channel->socket_.cancel();
+    }
+    void stop(){
+        for(auto& channel : channels_){
+            channel->socket_.cancel();
+        }
+        io_context_.stop();
+        end_.get_future().get();
+    }
+    void set_read_callback(std::function<uint32_t(std::shared_ptr<_channel>,void*,uint32_t)> callback){
+        read_callback_ = callback;
+    }
+    void set_connection_callback(std::function<void(std::shared_ptr<_channel>,bool,const std::string&)> callback){
+        connection_callback_ = callback;
+    }
 
 private:
     void do_accept(){
         acceptor_.async_accept([this](boost::system::error_code ec, btcp::socket socket){
             if(!ec){
                 Channel channel = std::make_shared<_channel>(std::move(socket));
-                channel->remove_callback_ = std::bind(&InternalTcp::close_channel,this,std::placeholders::_1);
-                channel->read_callback_ = read_callback_;
-                channel->close_callback_ = close_callback_;
+                channel_init(channel);
                 channels_.insert(channel);
+                if (connection_callback_) {
+                    connection_callback_(channel,true,"");
+                }
                 channel->do_read();
             }
             do_accept();
         });
     }
+    void do_connect(const std::string& ip,int port,const std::string& flag = ""){
+        asio::ip::address address = asio::ip::address::from_string(ip);
+        btcp::endpoint endpoint(address,port);
+        std::shared_ptr<btcp::socket> socket = std::make_shared<btcp::socket>(io_context_);
+        socket->async_connect(endpoint,[this,socket,flag](boost::system::error_code ec){
+            if(!ec){
+                Channel channel = std::make_shared<_channel>(std::move(*socket));
+                channel_init(channel);
+                channels_.insert(channel);
+                if (connection_callback_) {
+                    connection_callback_(channel,true,flag);
+                }
+                channel->do_read();
+            }
+        });
+    }
+    void channel_init(Channel channel){
+        channel->read_callback_ = read_callback_;
+        channel->connection_callback_ = connection_callback_;
+        channel->remove_callback_ = std::bind(&InternalTcp::close_channel,this,std::placeholders::_1);
+    }
 
-private:
     void close_channel(Channel channel){
         channels_.erase(channel);
     }
@@ -121,8 +180,10 @@ private:
     asio::io_context io_context_;
     btcp::acceptor acceptor_;
     std::unordered_set<Channel> channels_;
-    std::function<int32_t(std::shared_ptr<_channel>,void*,uint32_t)> read_callback_;
-    std::function<void(std::shared_ptr<_channel>)> close_callback_;
+    std::function<uint32_t(std::shared_ptr<_channel>,void*,uint32_t)> read_callback_;
+    std::function<void(std::shared_ptr<_channel>,bool,const std::string&)> connection_callback_;
+    std::shared_ptr<std::thread> thread_;
+    std::promise<bool> end_;
 }; // class InternalTcp
 } // namespace tcp
 } // namespace common
--- server/gateway/main.cc
+++ server/gateway/main.cc
@@ -1,6 +1,22 @@
 
+#include <internal_tcp.hh>
+#include <iostream>
 
 int main() {
 
+    common::tcp::InternalTcp tcp(8080);
+    tcp.set_read_callback([](common::tcp::Channel channel,void* data,uint32_t size){
+        std::cout << "read " << size << " bytes" << std::endl;
+        channel->do_write(data,size);
+        return size;
+    });
+    tcp.set_connection_callback([](common::tcp::Channel channel,bool connected,const std::string& flag){
+        if(connected){
+            std::cout << "connection established" << std::endl;
+        }else {
+            std::cout << "connection closed" << std::endl;
+        }
+    });
+    tcp.sync_start();
     return 0;
 }
